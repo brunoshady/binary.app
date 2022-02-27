b870716898af7b6ceefe41fbe66507288e2f584c
Bruno Baehr <bruno.baehr@cppro.com.br>
2022-02-27 16:28:36 -0300

    add jsonhandler

---
+++ Utils/JsonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using BinaryApp.Model;
+
+namespace BinaryApp.Utils;
+
+public static class JsonHandler
+{
+    public static object Deserialize(string json)
+    {
+        var authorizeResponse = JsonSerializer.Deserialize<AuthorizeResponse>(json);
+
+        if (authorizeResponse is {Authorize: { }})
+            return authorizeResponse.Authorize;
+
+        var tickResponse = JsonSerializer.Deserialize<TickResponse>(json);
+
+        if (tickResponse is {Tick: { }})
+            return tickResponse.Tick;
+        
+        return null;
+    }
+
+    public static string Serialize(object @object)
+    {
+        return JsonSerializer.Serialize(@object);
+    }
+}
